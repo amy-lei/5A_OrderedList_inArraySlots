ef67081904674b106a093ed1c6cfc62e0b2ae9c9
Amy Lei <amy.lei2001@yahoo.com>
2019-03-31 23:03:30 -0400

    completed hw5a

--- OrderedList_inArraySlots.java
+++ OrderedList_inArraySlots.java
@@ -1,72 +1,78 @@
 public class OrderedList_inArraySlots{
-	// ==================================================
-	// FIELDS
-	private List_inArraySlots list; 
-	
-	
-	
+    // ==================================================
+    // FIELDS
+    private List_inArraySlots list; 
+    
+    
+    
 
-	// ==================================================
-	// CONSTRUCTORS
-	public OrderedList_inArraySlots(){
-		list = new List_inArraySlots(); // instantiate empty list 
-	}
-
-
+    // ==================================================
+    // CONSTRUCTORS
+    public OrderedList_inArraySlots(){
+        list = new List_inArraySlots(); // instantiate empty list 
+    }
 
 
-	// ==================================================
-	// METHODS
-	
-	/**
-	  @return string representation of ordered list
-	  
-	  format: [ element0, element1, element2]
-			
-	*/
-	public String toString(){
-		return "";
-	}
-	
-	/**
-	  @returns index of given value
-	*/ 
-	public int getIndex( int val){
-		return 0;
-	}
-	
-	/**
-	  @return value of the Object at given index 
-	 */
-	public int get(int index){
-		return 0;
-	}
-	
-	/** 
-	  Appends @value in the right spot
-	*/ 
-	public void add( int value){
-	}
-	
-	/**
-	  Removes given @value from the list
-	  
-	  @return value removed, by convention
-	*/
-	public int remove( int value) {
-		return value;
-	}
-	
-	/** 
-	  Removes value at given @index
-	  
-	  @return value removed, by convention
-	*/
-	public int removeAtIndex( int index){
-		return 0;
-	}
-	
-	
-	
-		
+
+
+    // ==================================================
+    // METHODS
+    
+    /**
+      @return the number of elements in this list     
+     */
+    public int size(){
+        return list.size();
+    }
+
+    /**
+      @return string representation of ordered list
+      
+      format: [ element0, element1, element2]
+            
+    */
+    public String toString(){
+        return list.toString();
+    }
+    
+    /** 
+      Appends @value in the right spot
+    */ 
+    public void add( int value){
+        // if empty list, just add
+        if (size() == 0) list.add(value);
+        
+        else{ int index;
+              for( index = 0 ; index < size() ; index++ ){ 
+                // add once at appropriate index
+                if (value <= list.get(index)) { 
+                    list.add( index, value);
+                    return; // to exit forloop
+                    }                    
+                }
+              
+              // reached end of the list? add at end 
+              list.add( index, value);
+        }
+        
+    }
+    
+    
+    /**
+     @return value corresponding to the given index
+     */
+    public int get(int index){
+        return list.get(index);
+    }
+        
+    /** 
+      Removes value at given @index
+      
+      @return value removed, by convention
+    */
+    public int remove( int index){
+        return list.remove(index);
+    }
+    
+        
 }
--- UserOfOrderedList.java
+++ UserOfOrderedList.java
@@ -1,11 +1,85 @@
 public class UserOfOrderedList{
-	public void main(String[] arg){
-		// test toString() by first instantiating an empty list
-		OrderedList_inArraySlots list = new OrderedList_inArraySlots();
-		System.out.println( "empty list: " + list);
-		
-		// test add( value)
-		
-		
-	}
+    public static void main(String[] arg){
+        OrderedList_inArraySlots list = new OrderedList_inArraySlots();
+        
+        // test toString and size
+        System.out.println( "Testing toString() and size()");
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            + System.lineSeparator()
+                            );
+       
+       // test add
+        System.out.println( "Testing add(val)");
+        list.add( 23); // add to empty list
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );
+        list.add( 4); // add to beginning
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );
+        list.add( 10); // add to middle
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );
+        list.add( 19); // add to middle
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );
+        list.add( 100); // add to end
+        System.out.println( "numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );
+        System.out.println( System.lineSeparator());
+        
+        // test get
+        System.out.println( "Testing get(index)");
+        System.out.println( list);
+        System.out.println( "element number 0: " 
+                            + list.get(0)
+                            );
+        System.out.println( "element number 2: " 
+                            + list.get(2)
+                            );
+        System.out.println( "last element: " 
+                            + list.get( list.size() -1)
+                            );
+        System.out.println( System.lineSeparator());
+
+        
+       // test remove
+        System.out.println( "Testing remove(index)");
+        list.remove( 0); // remove from beginning
+        System.out.println( "After removing element0, numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );       
+        list.remove( 2); // remove from middle
+        System.out.println( "After removing element2, numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );       
+        list.remove( list.size() - 1); // remove from end
+        System.out.println( "After removing last element, numer of elements: " 
+                            + list.size() 
+                            + " list: "
+                            + list
+                            );       
+        
+    }
 }
